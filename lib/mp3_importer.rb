06fadd16b8eb7f9950bd7df51e564da6013b49d2
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-12 01:12:51 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,12 @@
 class MP3Importer
 
   attr_accessor :path, :files
-  
+
   def initialize(path)
     @path = path
+    @file = Dir.entries(path).grep(/.*\.mp3/)
   end
 
-  def files
-    @files = Dir["#{path}/*.mp3"]
-  end
 
   def import
   end
